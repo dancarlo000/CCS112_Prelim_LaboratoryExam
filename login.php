0ee756764a28948411c74effbdde85e58cf30a0e
Ruka-300ms <andreipellana1014.com>
2025-09-25 13:06:25 +0800

    this is login

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Log In</title>
+  <style>
+    body {
+      margin: 0;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: linear-gradient(135deg, #74ebd5, #ACB6E5); 
+      font-family: Arial, sans-serif;
+    }
+    .box {
+      background: white;
+      padding: 40px;
+      border-radius: 15px;
+      box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
+      text-align: center;
+    }
+    h2 {
+      margin-bottom: 20px;
+    }
+    .btn {
+      display: inline-block;
+      padding: 12px 24px;
+      margin: 10px;
+      border: none;
+      border-radius: 8px;
+      background-color: #4CAF50;a
+      color: white;
+      font: white;
+      font-size: 16px;
+      cursor: pointer;
+      text-decoration: none;
+      transition: background 0.3s ease;
+    }
+    .btn:hover {
+      background-color: #07660bff;
+    }
+
+    a {
+      color: white;
+    }
+</style>
+
+</head>
+<body>
+  <div class="box">
+    <h2>Select User Type</h2>
+    <a href="userCatalog.php" class="btn">Student</a>
+    <a href="adminCatalog.php" class="btn">Admin</a>
+  </div>
+</body>
+</html>
